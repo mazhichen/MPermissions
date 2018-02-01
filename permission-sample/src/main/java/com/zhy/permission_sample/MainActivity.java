59c6453fe8c0b1c1a6ed82b4cc5fd8ace65076ca
mazhichen <bjpeiwg@163.com>
2018-02-01 22:07:09 +0800

    第一次提交

--- permission-sample/src/main/java/com/zhy/permission_sample/MainActivity.java
+++ permission-sample/src/main/java/com/zhy/permission_sample/MainActivity.java
@@ -12,49 +12,41 @@ import com.zhy.m.permission.PermissionDenied;
 import com.zhy.m.permission.PermissionGrant;
 import com.zhy.m.permission.ShowRequestPermissionRationale;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
     private Button mBtnSdcard, mBtnCallPhone;
     private static final int REQUECT_CODE_SDCARD = 2;
     private static final int REQUECT_CODE_CALL_PHONE = 3;
-
+    private static final int TESTGETCOMMIT = 9;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mBtnSdcard = (Button) findViewById(R.id.id_btn_sdcard);
         mBtnCallPhone = (Button) findViewById(R.id.id_btn_callphone);
 
-        mBtnSdcard.setOnClickListener(new View.OnClickListener()
-        {
+        mBtnSdcard.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
 
-                if (!MPermissions.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUECT_CODE_SDCARD))
-                {
+                if (!MPermissions.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE, REQUECT_CODE_SDCARD)) {
                     MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_SDCARD, Manifest.permission.WRITE_EXTERNAL_STORAGE);
                 }
             }
         });
 
-        mBtnCallPhone.setOnClickListener(new View.OnClickListener()
-        {
+        mBtnCallPhone.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_CALL_PHONE, Manifest.permission.CALL_PHONE);
             }
         });
     }
 
     @ShowRequestPermissionRationale(REQUECT_CODE_SDCARD)
-    public void whyNeedSdCard()
-    {
+    public void whyNeedSdCard() {
         Toast.makeText(this, "I need write news to sdcard!", Toast.LENGTH_SHORT).show();
         MPermissions.requestPermissions(MainActivity.this, REQUECT_CODE_SDCARD, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
@@ -62,35 +54,30 @@ public class MainActivity extends AppCompatActivity
 
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         MPermissions.onRequestPermissionsResult(this, requestCode, permissions, grantResults);
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
 
     @PermissionGrant(REQUECT_CODE_SDCARD)
-    public void requestSdcardSuccess()
-    {
+    public void requestSdcardSuccess() {
         Toast.makeText(this, "GRANT ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
     }
 
     @PermissionDenied(REQUECT_CODE_SDCARD)
-    public void requestSdcardFailed()
-    {
+    public void requestSdcardFailed() {
         Toast.makeText(this, "DENY ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
     }
 
 
     @PermissionGrant(REQUECT_CODE_CALL_PHONE)
-    public void requestCallPhoneSuccess()
-    {
+    public void requestCallPhoneSuccess() {
         Toast.makeText(this, "GRANT ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
     }
 
     @PermissionDenied(REQUECT_CODE_CALL_PHONE)
-    public void requestCallPhoneFailed()
-    {
+    public void requestCallPhoneFailed() {
         Toast.makeText(this, "DENY ACCESS SDCARD!", Toast.LENGTH_SHORT).show();
     }
 
